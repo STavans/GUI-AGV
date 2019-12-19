00d36d0b0ecf7d87fb11beb6c3898d6da4dcfc2b
sebas <mab.tromper@student.avans.nl>
2019-12-19 13:05:16 +0100

    created buttons for route + observablelist, made arraylist to store commands,

--- src/bluebotclient/frontend/MainMenu.java
+++ src/bluebotclient/frontend/MainMenu.java
@@ -1,11 +1,19 @@
 package bluebotclient.frontend;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -14,9 +22,13 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.awt.*;
 import java.util.List;
 
 public class MainMenu extends Application {
+
+    private ObservableList<String> route =
+            FXCollections.observableArrayList();
 
     public void start(Stage stage) {
         BorderPane borderPane = new BorderPane();
@@ -43,7 +55,7 @@ public class MainMenu extends Application {
         borderPane.setTop(menuBar);
 
         Label changeLog = createLabel();
-        BorderPane route = createRoute();
+        BorderPane route = Route();
         ListView routeList = createListView();
         BorderPane pcControl = createControls();
 
@@ -55,26 +67,6 @@ public class MainMenu extends Application {
         borderPane.setCenter(changeLog);
 
         Scene scene = new Scene(borderPane, 800, 600);
-
-//        scene.setOnKeyPressed(event -> {
-//            switch (event.getCode()) {
-//                case UP:    goNorth = true; break;
-//                case DOWN:  goSouth = true; break;
-//                case LEFT:  goWest  = true; break;
-//                case RIGHT: goEast  = true; break;
-//                case SHIFT: running = true; break;
-//            }
-//        });
-//
-//        scene.setOnKeyReleased(event -> {
-//            switch (event.getCode()) {
-//                case UP:    goNorth = false; break;
-//                case DOWN:  goSouth = false; break;
-//                case LEFT:  goWest  = false; break;
-//                case RIGHT: goEast  = false; break;
-//                case SHIFT: running = false; break;
-//            }
-//        });
 
         stage.initStyle(StageStyle.UTILITY);
         stage.setScene(scene);
@@ -88,17 +80,67 @@ public class MainMenu extends Application {
         return label;
     }
 
-    private BorderPane createRoute(){
+    public BorderPane Route(){
         BorderPane pane = new BorderPane();
 
         Label label = new Label("Add command:");
-        TextField field = new TextField();
-        Button button = new Button("Add");
-        TextArea area = new TextArea();
+        Button button = new Button("Stop");
+        Button button1 = new Button("Forward");
+        Button button2 = new Button("Backwards");
+        Button button3 = new Button("Left");
+        Button button4 = new Button("Right");
+        Button button5 = new Button("Delete");
+        Button button6 = new Button("Save");
+        ListView view = new ListView();
+
+        button.setOnAction(actionEvent -> {
+            view.getItems().add("Stop");
+            route.add("Stop");
+            System.out.println();
+        });
+
+        button1.setOnAction(actionEvent -> {
+            view.getItems().add("Forward");
+            route.add("Forward");
+        });
+
+        button2.setOnAction(actionEvent -> {
+            view.getItems().add("Backwards");
+            route.add("Backwards");
+        });
+
+        button3.setOnAction(actionEvent -> {
+            view.getItems().add("Left");
+            route.add("Left");
+        });
+
+        button4.setOnAction(actionEvent -> {
+            view.getItems().add("Right");
+            route.add("Right");
+        });
+
+        button5.setOnAction(actionEvent -> {
+            int selectedIndices = view.getSelectionModel().getSelectedIndex();
+            if (selectedIndices != -1) {
+
+                view.getItems().remove(selectedIndices);
+                route.remove(selectedIndices);
+            }
+        });
+
+        button6.setOnAction(actionEvent -> {
+            for (int i = 0; i < route.size(); i++){
+                System.out.println(route.get(i));
+            }
+        });
+
+        HBox hBox = new HBox();
+        hBox.setSpacing(10);
+        hBox.getChildren().addAll(button1, button2, button3, button4, button);
 
         VBox vBox = new VBox();
         vBox.setSpacing(10);
-        vBox.getChildren().addAll(label, field, button, area);
+        vBox.getChildren().addAll(label, hBox, view, button6, button5);
 
         pane.setLeft(vBox);
 
